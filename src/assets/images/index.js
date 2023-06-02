5270e0ad6e703e81f27855393f62b8d1dbb27213
Nathan Cao <nhatcao0111@gmail.com>
2023-06-02 12:16:14 +0700

    import logo fontawesome - header

---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+const images = {
+  logo: require('./clean-logo.svg').default,
+};
+
+export default images;
--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,9 +1,21 @@
 import styles from './Header.module.scss';
+import images from '../../../../assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   return (
     <header className={styles.wrapper}>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <img src={images.logo} alt="Clean logo"></img>
+        </div>
+        <nav className={styles.nav}>
+          <button className={styles.cart}>
+            <FontAwesomeIcon icon={faShoppingCart} />
+          </button>
+        </nav>
+      </div>
     </header>
   );
 }
