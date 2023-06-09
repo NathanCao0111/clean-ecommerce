81bef372ac4e8d3f9a79a0545c94667170affb5a
Nathan Cao <nhatcao0111@gmail.com>
2023-06-09 12:42:18 +0700

    update footer

--- src/components/Layout/DefaultLayout/Footer/index.js
+++ src/components/Layout/DefaultLayout/Footer/index.js
@@ -1,7 +1,23 @@
 import styles from './Footer.module.scss';
+import { Link } from 'react-router-dom';
 
 function Footer() {
-  return <footer className={styles.wrapper}></footer>;
+  return (
+    <footer className={styles.wrapper}>
+      <div className={styles.links}>
+        <Link to="#">About</Link>
+        <Link to="#">Store locator</Link>
+        <Link to="#">FAQs</Link>
+        <Link to="#">News</Link>
+        <Link to="#">Careers</Link>
+        <Link to="#">Contact Us</Link>
+      </div>
+      <p className={styles.copyright}>
+        Design by &nbsp;
+        <a href="https://github.com/Abderraouf-Rahmani">Abderraouf</a>
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
