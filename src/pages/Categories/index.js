1a3ce3cca326d41b639bd5bc1ec76ed9580410d1
Nathan Cao <nhatcao0111@gmail.com>
2023-06-11 18:06:53 +0700

    update content - categories

--- src/pages/Categories/index.js
+++ src/pages/Categories/index.js
@@ -1,11 +1,27 @@
 import styles from './Categories.module.scss';
 import Sidebar from '../../components/Layout/components/Sidebar';
+import Proud from '../../components/Layout/components/Proud';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 function Categories() {
   return (
     <section className={styles.container}>
       <Sidebar />
-      <div className={styles.content}></div>
+      <div className={styles.content}>
+        <div className={styles.header}>
+          <h3>ALL</h3>
+          <div className={styles.search}>
+            <form>
+              <input type="text" spellCheck={false} placeholder="Search products" />
+              <button>
+                <FontAwesomeIcon icon={faSearch} />
+              </button>
+            </form>
+          </div>
+        </div>
+        <Proud />
+      </div>
     </section>
   );
 }
