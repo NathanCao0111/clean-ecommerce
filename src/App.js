c7f5b8c8b5ecbfc1602e68fa7d02ca6189189eef
Nathan Cao <nhatcao0111@gmail.com>
2023-06-15 23:10:04 +0700

    update login page

--- src/App.js
+++ src/App.js
@@ -2,29 +2,32 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { publicRoutes } from './routes';
 import { DefaultLayout } from './components/Layout';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          {publicRoutes.map((element, index) => {
-            const Page = element.component;
-            return (
-              <Route
-                key={index}
-                path={element.path}
-                element={
-                  <DefaultLayout>
-                    <Page />
-                  </DefaultLayout>
-                }
-              />
-            );
-          })}
-        </Routes>
-      </div>
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            {publicRoutes.map((element, index) => {
+              const Page = element.component;
+              return (
+                <Route
+                  key={index}
+                  path={element.path}
+                  element={
+                    <DefaultLayout>
+                      <Page />
+                    </DefaultLayout>
+                  }
+                />
+              );
+            })}
+          </Routes>
+        </div>
+      </Router>
+    </ChakraProvider>
   );
 }
 
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,47 @@
+import styles from './Login.module.scss';
+import { FormControl, FormLabel, Input, FormHelperText, FormErrorMessage } from '@chakra-ui/react';
+import { useState } from 'react';
+
 function Login() {
-  return <h1>Login</h1>;
+  const [userData, setUserData] = useState({
+    email: '',
+    password: '',
+  });
+
+  console.log(userData);
+
+  return (
+    <div>
+      <FormControl id="email">
+        <FormLabel>Email address</FormLabel>
+        <Input
+          type="email"
+          value={userData.email}
+          onChange={(e) =>
+            setUserData((prev) => ({
+              ...prev,
+              email: e.target.value,
+            }))
+          }
+        />
+        <FormHelperText>We'll never share your email.</FormHelperText>
+      </FormControl>
+      <FormControl id="password">
+        <FormLabel>Password</FormLabel>
+        <Input
+          type="password"
+          value={userData.password}
+          onChange={(e) =>
+            setUserData((prev) => ({
+              ...prev,
+              password: e.target.value,
+            }))
+          }
+        />
+        <FormHelperText>We'll never share your email.</FormHelperText>
+      </FormControl>
+    </div>
+  );
 }
 
 export default Login;
