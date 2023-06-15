854dd71070ea163720876c085c2230abf177b280
Nathan Cao <nhatcao0111@gmail.com>
2023-06-15 15:14:46 +0700

    update search input - categories

--- src/pages/Categories/index.js
+++ src/pages/Categories/index.js
@@ -3,12 +3,29 @@ import Sidebar from '../../components/Layout/components/Sidebar';
 import Proud from '../../components/Layout/components/Proud';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import useProducts from '../../context/useProducts';
 
 function Categories() {
   const productsData = useProducts();
   const [, setCategoryProducts] = productsData.categoryProductsContext;
+  const [products] = productsData.productsContext;
+  const [, setUpdatedProducts] = productsData.updatedProductsContext;
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleSearchBtn = () => {
+    const res = products.filter((element) => {
+      return element.title.toLowerCase().includes(searchInput.toLowerCase());
+    });
+    setUpdatedProducts(res);
+  };
+
+  const handleSearchInput = (e) => {
+    setSearchInput(e.target.value);
+    if (e.target.value === '') {
+      setUpdatedProducts(products);
+    }
+  };
 
   useEffect(() => {
     setCategoryProducts(true);
@@ -21,9 +38,15 @@ function Categories() {
         <div className={styles.header}>
           <h3>ALL</h3>
           <div className={styles.search}>
-            <form>
-              <input type="text" spellCheck={false} placeholder="Search products" />
-              <button>
+            <form onSubmit={(e) => e.preventDefault()}>
+              <input
+                type="text"
+                spellCheck={false}
+                placeholder="Search products"
+                value={searchInput}
+                onChange={handleSearchInput}
+              />
+              <button onClick={handleSearchBtn} type="submit">
                 <FontAwesomeIcon icon={faSearch} />
               </button>
             </form>
