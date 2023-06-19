571da6e16354be11159e4bed1b0b1d43dcbe51e0
Nathan Cao <nhatcao0111@gmail.com>
2023-06-19 22:35:27 +0700

    update auth pages logic

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,57 +1,68 @@
 import styles from './Login.module.scss';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 function Login() {
-  const [userData, setUserData] = useState({
-    email: '',
-    password: '',
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    password: Yup.string().min(8, 'Password must be at least 8 characters').required('Password is required'),
   });
 
-  const handleLoginBtn = () => {};
+  const initialValues = {
+    email: '',
+    password: '',
+  };
 
-  console.log(userData);
+  const navigate = useNavigate();
+
+  const handleLoginSubmit = (values) => {
+    const { email, password } = values;
+    console.log(values);
+    fetch('https://6448a5c1e7eb3378ca32d196.mockapi.io/api/clean-ecommerce/users')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const validateUser = data.filter((element) => {
+          if (element.email === email && element.password === password) {
+            console.log(element);
+            return element;
+          }
+          return;
+        });
+        console.log(validateUser);
+        if (validateUser.length !== 0) {
+          navigate('/');
+        }
+      });
+  };
 
   return (
     <section className={styles.container}>
       <div className={styles.content}>
         <h3>LOGIN</h3>
-        <div className={styles.inputContainer}>
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            id="email"
-            spellCheck={false}
-            value={userData.email}
-            onChange={(e) =>
-              setUserData((prev) => ({
-                ...prev,
-                email: e.target.value,
-              }))
-            }
-          />
-        </div>
-        <div className={styles.inputContainer}>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            spellCheck={false}
-            value={userData.password}
-            onChange={(e) =>
-              setUserData((prev) => ({
-                ...prev,
-                password: e.target.value,
-              }))
-            }
-          />
-        </div>
-        <button onClick={handleLoginBtn}>LOGIN</button>
-        <p>
-          <Link to="/signup">
-            Don't have an account? <span>Sign up</span>
-          </Link>
-        </p>
+        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleLoginSubmit}>
+          {({ errors, touched }) => (
+            <Form>
+              <div className={styles.inputContainer}>
+                <label htmlFor="email">Email address</label>
+                <Field id="email" name="email" type="email" placeholder="E.g. johnsmith@gmail.com" spellCheck={false} />
+                <ErrorMessage name="email">{(msg) => <div className={styles.error}>{msg}</div>}</ErrorMessage>
+              </div>
+              <div className={styles.inputContainer}>
+                <label htmlFor="password">Password</label>
+                <Field id="password" name="password" type="password" />
+                <ErrorMessage name="password">{(msg) => <div className={styles.error}>{msg}</div>}</ErrorMessage>
+              </div>
+              <button type="submit">LOGIN</button>
+              <p>
+                <Link to="/signup">
+                  Don't have an account? <span>Sign up</span>
+                </Link>
+              </p>
+            </Form>
+          )}
+        </Formik>
       </div>
     </section>
   );
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,73 +1,101 @@
 import styles from './Signup.module.scss';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import clsx from 'clsx';
+import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmarkSquare } from '@fortawesome/free-solid-svg-icons';
 
 function Signup() {
-  const [userData, setUserData] = useState({
-    email: '',
-    password: '',
-    confirmPassword: '',
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().min(2, 'Too short!').max(70, 'Too long!').required('Name is required'),
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    password: Yup.string().min(8, 'Password must be at least 8 characters').required('Password is required'),
   });
 
-  const handleSignupBtn = () => {};
+  const initialValues = {
+    name: '',
+    email: '',
+    password: '',
+  };
 
-  console.log(userData);
+  const navigate = useNavigate();
+  const [userMatchWarning, setUserMatchWarning] = useState(false);
+
+  useEffect(() => {
+    if (userMatchWarning === true) {
+      const timer = setTimeout(() => {
+        setUserMatchWarning(false);
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [userMatchWarning]);
+
+  const handleSignupSubmit = async (values) => {
+    const { name, email, password } = values;
+
+    const response = await fetch('https://6448a5c1e7eb3378ca32d196.mockapi.io/api/clean-ecommerce/users');
+    const data = await response.json();
+    const validateEmail = data.map((element) => element.email);
+    if (validateEmail.includes(email)) {
+      setUserMatchWarning(true);
+      return;
+    }
+
+    await fetch('https://6448a5c1e7eb3378ca32d196.mockapi.io/api/clean-ecommerce/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, password }),
+    });
+
+    navigate('/login');
+  };
+
+  console.log(userMatchWarning);
 
   return (
     <section className={styles.container}>
+      <div className={clsx(styles.notify, userMatchWarning === true ? styles.slideIn : '')}>
+        <p>
+          Email address has already existed &nbsp;
+          <span>
+            <FontAwesomeIcon icon={faXmarkSquare} />
+          </span>
+        </p>
+      </div>
       <div className={styles.content}>
         <h3>SIGN UP</h3>
-        <div className={styles.inputContainer}>
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            id="email"
-            spellCheck={false}
-            value={userData.email}
-            onChange={(e) =>
-              setUserData((prev) => ({
-                ...prev,
-                email: e.target.value,
-              }))
-            }
-          />
-        </div>
-        <div className={styles.inputContainer}>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            spellCheck={false}
-            value={userData.password}
-            onChange={(e) =>
-              setUserData((prev) => ({
-                ...prev,
-                password: e.target.value,
-              }))
-            }
-          />
-        </div>
-        <div className={styles.inputContainer}>
-          <label htmlFor="confirmPassword">Confirm password</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            spellCheck={false}
-            value={userData.confirmPassword}
-            onChange={(e) =>
-              setUserData((prev) => ({
-                ...prev,
-                confirmPassword: e.target.value,
-              }))
-            }
-          />
-        </div>
-        <button onClick={handleSignupBtn}>SIGN UP</button>
-        <p>
-          <Link to="/login">
-            Already have an account? <span>Log in</span>
-          </Link>
-        </p>
+        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSignupSubmit}>
+          {({ errors, touched }) => (
+            <Form>
+              <div className={styles.inputContainer}>
+                <label htmlFor="name">Name</label>
+                <Field id="name" name="name" type="text" placeholder="E.g. John Smith" spellCheck={false} />
+                <ErrorMessage name="name">{(msg) => <div className={styles.error}>{msg}</div>}</ErrorMessage>
+              </div>
+              <div className={styles.inputContainer}>
+                <label htmlFor="email">Email address</label>
+                <Field id="email" name="email" type="email" placeholder="E.g. johnsmith@gmail.com" spellCheck={false} />
+                <ErrorMessage name="email">{(msg) => <div className={styles.error}>{msg}</div>}</ErrorMessage>
+              </div>
+              <div className={styles.inputContainer}>
+                <label htmlFor="password">Password</label>
+                <Field id="password" name="password" type="password" />
+                <ErrorMessage name="password">{(msg) => <div className={styles.error}>{msg}</div>}</ErrorMessage>
+              </div>
+              <button type="submit">SIGN UP</button>
+              <p>
+                <Link to="/login">
+                  Already have an account? <span>Log in</span>
+                </Link>
+              </p>
+            </Form>
+          )}
+        </Formik>
       </div>
     </section>
   );
